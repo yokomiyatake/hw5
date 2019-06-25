5b6e1795cf1e14a74e22340bae092adad90d43c5
yoko <miyatakeyouko@gmail.com>
2019-06-25 11:45:46 +0900

    first commit of yoko

--- go-hello/app.go
+++ go-hello/app.go
@@ -6,10 +6,25 @@ import (
 )
 
 func init() {
-	http.HandleFunc("/", handlePata)
+	http.HandleFunc("/", getWords)
+	http.HandleFunc("/test", showPata)
 }
 
-func handlePata(w http.ResponseWriter, r *http.Request) {
+func getWords(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	fmt.Fprintf(w, "Hello world!\n")
+
+    fmt.Fprintf(w, "<form>")
+    fmt.Fprintf(w, "word1:<br>")
+    fmt.Fprintf(w, "<input type='text' name='word1' value='パトカー'><br>")
+    fmt.Fprintf(w, "word2:<br>")
+    fmt.Fprintf(w, "<input type='text' name='word2' value='タクシー'><br><br>")
+    fmt.Fprintf(w, "<input type='submit' value='合体'>")
+    fmt.Fprintf(w, "</form>")
 }
+
+func showPata(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+
+	fmt.Fprintf(w, "<p>ぱたとくかしーー</p>")
+}
+
