878e979b766270010071380a9c3d82a274e8be10
yoko <miyatakeyouko@gmail.com>
2019-06-26 12:35:57 +0900

    implement パタトクカシーー

--- go-hello/app.go
+++ go-hello/app.go
@@ -12,13 +12,30 @@ func init() {
 func getWords(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 
-    fmt.Fprintf(w, "<form>")
+	fmt.Fprintf(w, "<head>")
+	fmt.Fprintf(w, "<script type='text/javascript' language='javascript'>")
+	fmt.Fprintf(w, "function onButtonClick() {")
+	fmt.Fprintf(w, "		showField = document.getElementById('output');")
+	fmt.Fprintf(w, "		text1 = document.forms.inputForm.textBox1.value;")
+	fmt.Fprintf(w, "		text2 = document.forms.inputForm.textBox2.value;")
+	fmt.Fprintf(w, "		var pata = '';")
+	fmt.Fprintf(w, "		for (var i = 0; i < text1.length; i++) {")
+	fmt.Fprintf(w, "			pata += text1.charAt(i);")
+	fmt.Fprintf(w, "			pata += text2.charAt(i);")
+	fmt.Fprintf(w, "		}")
+	fmt.Fprintf(w, "		showField.innerText = pata;")
+	fmt.Fprintf(w, "}")
+	fmt.Fprintf(w, "</script>")
+	fmt.Fprintf(w, "</head>")
+
+    fmt.Fprintf(w, "<form name='form1' id='inputForm' action=''>")
     fmt.Fprintf(w, "word1:<br>")
-    fmt.Fprintf(w, "<input type='text' name='word1' value='パトカー'><br>")
+    fmt.Fprintf(w, "<input type='text' name='word1' id='textBox1' value='パトカー' /><br>")
     fmt.Fprintf(w, "word2:<br>")
-    fmt.Fprintf(w, "<input type='text' name='word2' value='タクシー'><br><br>")
-    fmt.Fprintf(w, "<input type='submit' value='合体'>")
+    fmt.Fprintf(w, "<input type='text' name='word2' id='textBox2' value='タクシー' /><br><br>")
+    fmt.Fprintf(w, "<input type='button' value='合体' onclick='onButtonClick();' />")
     fmt.Fprintf(w, "</form>")
+	fmt.Fprintf(w, "<div id='output'></div>")
 }
 
 
